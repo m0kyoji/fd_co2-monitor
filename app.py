37ec37577bda3d6283ea8c3a66a87ea78e783b3b
kyoji <moriguchi@fundely.co.jp>
2024-12-02 11:41:41 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
+# スプレッドシートに接続
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# データの読み込み
+df = conn.read()
+
+# タイムスタンプ列を日付型に変換
+df['タイムスタンプ'] = pd.to_datetime(df['タイムスタンプ'])
+
+# グラフの描画
+st.title('環境データ可視化')
+
+st.line_chart(df.set_index('タイムスタンプ')[['気温', '湿度', 'CO2濃度']])
+
+# データテーブルの表示（オプション）
+st.dataframe(df)
